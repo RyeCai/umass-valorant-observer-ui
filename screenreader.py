49a1b8dfd8173937392132334f84431c7c14e06a
Capta <Captaincow285@gmail.com>
2022-10-09 00:55:55 -0400

    some openCV experiements

--- screenreader.py
+++ screenreader.py
@@ -6,6 +6,14 @@ import mouse
 import keyboard
 import datetime
 import time
+import os
+
+
+#Variables that need to be changed on update
+agentList = ["Astra", "Breach", "Brimstone", "Chamber", "Cypher", "Fade", "Jett", "KayO", "Killjoy", "Neon", "Omen", "Phoenix", "Raze", "Reyna", "Sage", "Skye", "Sova", "Viper", "Yoru"]
+
+
+dirname = os.path.dirname(__file__)
 
 
 def produceGameState(alive = 10):
@@ -33,10 +41,38 @@ def produceGameState(alive = 10):
         utility = image[910:1080, 530:1200]
         cv2.imwrite("agentUtility" + str(i) + ".png", utility)
 
+#this function generates a file path for the identifyAgentHUD function to use
+def generateAssetPath(arrayElement):
+    partialPath = "/src/assets/agents/" + arrayElement + "/abilities/Ultimate.png"
+    return "C:/Users/Capta/Documents/Visual Studio 2017/UMass-Valorant-Observer-UI" + partialPath #hardcoded for testing
+
+def identifyAgentHUD(pointOfViewNumber = 0):
+    #for agentListElement in agentList:
+    agentListElement = "Brimstone"
+    print(generateAssetPath(agentListElement))
+    ultimateImage = cv2.imread(generateAssetPath(agentListElement), 0)
+    w, h = ultimateImage.shape[::-1]
+        #ultimateImage = cv2.cvtColor(ultimateImage, cv2.COLOR_BGR2GRAY) 
+    imageAnalysis = cv2.imread("C:/Users/Capta/Downloads/testSS.png") #hardcoded path for testing
+    cv2.imshow("pre", imageAnalysis)
+    imageAnalysis = cv2.cvtColor(imageAnalysis, cv2.COLOR_BGR2GRAY)
+
+    res = cv2.matchTemplate(imageAnalysis, ultimateImage, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(res >- 0.8)
+
+        #this displays the image, is temporary troubleshooting step
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(imageAnalysis, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
+    cv2.imshow('detected', imageAnalysis)
+    cv2.waitKey(0)
+
+
 while True:
     if keyboard.read_key() == "p":
         produceGameState()
         now = str(datetime.datetime.now())
         print("Produced Game State at " + now)
+    elif keyboard.read_key() == "[":
+        identifyAgentHUD()
     elif keyboard.read_key() == "o":
         break
